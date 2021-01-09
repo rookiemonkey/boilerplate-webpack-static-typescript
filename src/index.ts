2d05bdb070c23f3cfe19d10a75c758dccc747655
rookiemonkey <kevinroirigorbasina@pm.me>
2021-01-09 09:20:24 +0800

    update scripts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-import moduleOne from './modules/module1';
-import moduleTwo from './modules/module2';
 
-console.log('This is from index.ts');
-
-moduleOne();
-
-moduleTwo();
+//! import main css/ts files
+import './assets/css/main.css';
+import './assets/ts/main.ts';
