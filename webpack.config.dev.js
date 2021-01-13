83f1cd247f6889f75ac0c7c8184561477dd1ac2c
rookiemonkey <kevinroirigorbasina@pm.me>
2021-01-14 06:34:52 +0800

    update webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,10 +1,27 @@
 const { merge } = require("webpack-merge");
 const common = require('./webpack.config.common');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
 
     mode: 'development',
 
-    devtool: 'eval-source-map'
+    devtool: 'eval-source-map',
+
+    optimization: {
+        minimize: false,
+        minimizer: [
+            new TerserPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: false,
+                extractComments: false,
+                terserOptions: {
+                    // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+                    comments: false,
+                }
+            })
+        ]
+    },
 
 });
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,10 +1,27 @@
 const { merge } = require("webpack-merge");
 const common = require('./webpack.config.common');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
 
     mode: 'production',
 
-    devtool: 'source-map'
+    devtool: 'source-map',
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true,
+                extractComments: false,
+                terserOptions: {
+                    // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+                    comments: false,
+                }
+            })
+        ]
+    },
 
 });
